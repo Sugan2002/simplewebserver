84526778a9f6a3e46302221b0170e419f569c85c
sugan2002 <suganya.parthiban2002@gmail.com>
2021-02-17 13:48:31 +0000

    comments

--- mywebserver.py
+++ mywebserver.py
@@ -1,22 +1,3 @@
-# Developing a Simple Webserver
-## AIM:
-To develop a simple webserver to serve html pages.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation
-### Step 2:
-Design of webserver workflow
-### Step 3:
-Implementation using Python code
-### Step 4:
-Serving the HTML pages.
-### Step 5:
-Testing the webserver
-
-## PROGRAM:
-
-```
 from http.server import HTTPServer,BaseHTTPRequestHandler
 
 Content="""
@@ -60,12 +41,3 @@ server_address=('',80)
 httpd = HTTPServer(server_address,myhandler)
 print("MY WEBSERVER IS RUNNING...")
 httpd.serve_forever()
-```
-
-
-## OUTPUT:
-
-![output](./static/img/r1.png)
-
-## RESULT:
-Thus a simple webserver is designed for to display multiplication table and is hosted in the URL http://suganya.student.saveetha.in .
